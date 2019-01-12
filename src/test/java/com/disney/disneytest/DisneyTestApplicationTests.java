2efa279176e4d3a4308434753b099a58b6a0dd5e
Chris Lehman <chrisalehman@gmail.com>
2019-01-12 14:14:02 -0800

    updates

--- src/test/java/com/disney/disneytest/DisneyTestApplicationTests.java
+++ src/test/java/com/disney/disneytest/DisneyTestApplicationTests.java
@@ -1,17 +1,16 @@
-//package com.disney.disneytest;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class DisneyTestApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
-//
+package com.disney.disneytest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DisneyTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
