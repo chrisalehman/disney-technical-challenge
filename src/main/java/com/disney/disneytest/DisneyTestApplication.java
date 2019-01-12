68941a9f4348c6672fe167df04e1575aa3cf38c4
Admin <admin@Jacobs-MacBook-Air.local>
2019-01-12 12:39:23 -0800

    prelim commit

---
+++ src/main/java/com/disney/disneytest/DisneyTestApplication.java
@@ -0,0 +1,14 @@
+package com.disney.disneytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DisneyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disney/disneytest/DisneyTestApplicationTests.java
@@ -0,0 +1,17 @@
+//package com.disney.disneytest;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class DisneyTestApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
+//
